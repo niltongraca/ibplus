467c0f926a3992416ab0f0d69cc81eecc61bad81
Nilton Graca <niltongraca@ibplus.ao>
2026-07-05 12:23:39 +0100

    fix: use @prisma/client instead of custom generated path for seed and lib

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "../src/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { neonConfig } from "@neondatabase/serverless";
 import WebSocket from "ws";
